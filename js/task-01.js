7e3d1d44e731baf0aa4f5fa2932cc9a42a31d312
Iryna <mefodievna2908@gmail.com>
2023-07-15 19:25:34 +0300

    task2

---
+++ js/task-01.js
@@ -0,0 +1,12 @@
+ const itemEl = document.querySelectorAll(`.item`);
+ itemEl.child("item")
+ console.log(itemEl);
+
+const titleEl = document.querySelector("h2").textContent;
+titleEl.querySelectorAll("H2")
+ console.log(titleEl.textContent);
+
+
+categories.forEach(function (number, index) {
+    console.log(`Індекс ${index}, значення ${number}`);
+  });
